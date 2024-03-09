95970de991e7088634128f1e42c3172bc8b224ca
pratiksha940 <patilpratiksha1008@gmail.com>
2024-03-09 13:03:41 +0530

    Practice with typeof operator and data types
    use the typeof operator and different data types.

---
+++ 02_typeOfAssign.js
@@ -0,0 +1,42 @@
+console.log("========= step 1 ==========");
+console.log("===== Before Initialization =====");
+var bankName;
+console.log("Name of bankName:",bankName);
+var typeOfbankName = typeof bankName;
+console.log("Data type of bankName:",typeOfbankName);
+console.log("===== After Initilization ======");
+bankName = "SBI Bank";
+console.log("Name of bankName:",bankName);
+typeOfbankName=typeof bankName;
+console.log("Data type of bankName:",typeOfbankName);
+
+console.log("======== step 2 ========");
+var marks='90%';
+var result = typeof marks;
+console.log("Value of marks:",marks);
+console.log("Data Type of marks:",result);
+
+console.log("======= step 3 =======");
+console.log("======= Before Initilization =======");
+var isWorking;
+console.log("Value of isWorking:",isWorking);
+var result=typeof isWorking;
+console.log("Data type of isWorking:",result);
+console.log("======== After Initilization =======");
+isWorking=true;
+result=typeof isWorking;
+console.log("Value of the isWorking:",isWorking);
+console.log("Data type of isWorking:",result);
+
+console.log("========= step 4 =========");
+var totalCount="Hundred and Seven";
+var result=typeof totalCount;
+console.log("Value of the totalCount:",totalCount);
+console.log("Data type of totalCount:",result);
+totalCount=107;
+result=typeof totalCount;
+console.log("Value of the totalCount:",totalCount);
+console.log("Data type of totalCount:",result);
+
+
+
